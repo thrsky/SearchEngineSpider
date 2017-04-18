df897b79a2a5ca5e211e1564f3bf0bd1e72380f0
thrsky <1491474343@qq.com>
2017-04-18 11:09:33 +0800

    增加获取工资和工作年限方法

--- article/utils/common.py
+++ article/utils/common.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import hashlib
+import hashlib,re
 
 def get_md5(url):
     if isinstance(url,str):
@@ -8,3 +8,31 @@ def get_md5(url):
     m=hashlib.md5()
     m.update(url)
     return m.hexdigest()
+
+def get_salary(value):
+    if value:
+        min_re=re.match('(\d+?)k-(\d+?)k',value)
+        if min_re:
+            min=(int(min_re.group(1))*1000)
+            max=(int(min_re.group(2))*1000)
+        else:
+            min=0
+            max=0
+        return min,max
+    else:
+        return 0,0
+
+def get_year(value):
+    if value:
+        year=re.match('经验(\d+?)-(\d+?)年',value)
+        if year:
+            min=int(year.group(1))
+            max=int(year.group(2))
+        else:
+            min=0
+            max=0
+        return min,max
+    else:
+        return 0,0
+
+print(get_year('经验1-3年 /'))
