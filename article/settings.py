254f811f434ca7aabcc1547a841eea04bbaab0a2
thrsky <1491474343@qq.com>
2017-04-20 15:41:28 +0800

    修复更新

--- article/settings.py
+++ article/settings.py
@@ -54,7 +54,7 @@ DEFAULT_REQUEST_HEADERS = {
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    #'article.utils.middlewares.RandomUserAgentMiddleware': 400,
+    'article.utils.middlewares.RandomUserAgentMiddleware': 400,
 }
 
 RAMDOM_UA_TYPE = "random"
