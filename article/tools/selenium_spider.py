d5b4d047622079598ba45ebe86c870c5881f2f9d
thrsky <1491474343@qq.com>
2017-04-21 16:27:47 +0800

    Chromedriver不加载图片 以及phantomjs的基本使用

--- article/tools/selenium_spider.py
+++ article/tools/selenium_spider.py
@@ -4,27 +4,61 @@ from selenium import webdriver
 from scrapy.selector import Selector
 from urllib.request import urlopen
 import re,random,os
-brower = webdriver.Chrome(executable_path="G:/Python/爬虫/scrapy/chromedriver.exe")
-image_path="E:/Picture"
-import time
-brower.get("http://www.polayoutu.com/collections")
-for i in range(3):
-    brower.execute_script("window.scrollTo(0,document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage")
-    time.sleep(3)
-t_selector=Selector(text=brower.page_source)
-image_url=t_selector.css(".download_original::attr(href)").extract()
-if image_url:
-    image_name = random.randint(10000, 20000)
-    res=urlopen(image_url)
-    if res.getcode()!=200:
-        pass
-    else:
-        data=res.read()
+# brower = webdriver.Chrome(executable_path="G:/Python/爬虫/scrapy/chromedriver.exe")
+# image_path="E:/Picture"
+# import time
+# brower.get("http://www.polayoutu.com/collections")
         
+#==================================================================
+"""
+    selenium使用基本流程
+    from selenium import webdriver
+    import scrapy.selector import Selector
+    brower=webdriver.Chrome(executable_path="下载好的对应浏览器的插件")  Chrome也要换成自己电脑上的浏览器
+    例如：#brower = webdriver.Chrome(executable_path="G:/Python/爬虫/scrapy/chromedriver.exe")
+    
+    brower.get("http://www.baidu.com")
+    text=brower.get_source #返回加载好后的网页代码
+    t_selector=Selector(text=text)  #用scrapy的Selector选择器来加载网页代码  方便以后用xpath和css选择信息
+    例如：t_selector.xpath('')
+         t_selector.css('')
+    
+"""
+#设置Chromedriver不加载图片
+chrome_opt=webdriver.ChromeOptions()
+prefs={"profile.managed_default_content_settings.images":2}
+chrome_opt.add_experimental_option("prefs",prefs)
+brower = webdriver.Chrome(executable_path="G:/Python/爬虫/scrapy/chromedriver.exe",chrome_options=chrome_opt)
+brower.get("https://www.taobao.com")
+
+
+#phantomjs 无界面的浏览器
+#多线程的情况下 phantomjs性能会下降很严重
+#在Linux下比较有用
+
+# phantomjs_path="你的phantomjs路径"
+# web=webdriver.PhantomJS(executable_path=phantomjs_path)
+# text=web.page_source
+# web.quit()
 
 
 
 
-print(image_url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # print(brower.page_source)
